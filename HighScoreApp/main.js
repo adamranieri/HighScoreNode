531f884257673391262d7f1b068e84d64a9bdd60
Adam_Ranieri <adam.ranieri@revature.com>
2020-01-30 16:29:30 -0500

    fixed comparator

--- HighScoreApp/main.js
+++ HighScoreApp/main.js
@@ -6,10 +6,6 @@ app.use(cors());
 
 const scores = [];
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname+'/index.html');
-  });
-
 // gets all scores
 app.get("/scores",(req,res)=>{
     res.send(JSON.stringify(scores))
@@ -26,8 +22,8 @@ app.get("/scores/:name/:points", (req,res)=>{
 
     
     let compare = (a, b) => {
-        if (a.points > b.points) return 1;
-        if (b.points > a.points) return -1;
+        if (a.points > b.points) return -1;
+        if (b.points > a.points) return 1;
         return 0;
       }
     
